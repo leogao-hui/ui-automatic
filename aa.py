dc3bdb060f2fb257f3001d6c2c9ea08038d0c088
leogao_hui <leogao_hui@163.com>
2020-03-03 09:13:35 +0800

    'update'

--- aa.py
+++ aa.py
@@ -22,27 +22,3 @@ import random
 # print(name)
 
 
-filter_add_user_data = {
-    'filter_add_user_data_one': {
-        'serial_num': '1_filter_serial_number',
-        'name': 'filterserialone',
-        'account': 'filterone'
-    },
-    'filter_add_user_data_two': {
-        'serial_num': '2_filter_serial_number',
-        'name': 'filterserialtwo',
-        'account': 'filtertwo'
-    },
-    'filter_add_user_data_three': {
-        'serial_num': '3_filter_serial_number',
-        'name': 'filterserialthree',
-        'account': 'filterthree'
-    },
-    'filter_add_user_data_four': {
-        'serial_num': '4_filter_serial_number',
-        'name': 'filterserialfour',
-        'account': 'filterfour'
-    },
-}
-
-for k in filter_add_user_data:
